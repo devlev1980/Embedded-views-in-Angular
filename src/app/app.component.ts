f7033db498f3cc4fe84e0e28e2758329c3602393
yevgenyl <yevgeny@consist.co.il>
2020-03-23 13:29:54 +0200

    add child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,13 @@ import {AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild, View
         <h1 class="display-1">Embedded views in Angular </h1>
         <h2 #title>I'm a title</h2>
         <button (click)="noClick()">Click Me!</button>
-        <app-child></app-child>
+        <app-child #childRef></app-child>
     `,
     styles: []
 })
 export class AppComponent implements AfterViewInit {
     @ViewChild('title') title: ElementRef;
+    @ViewChild('childRef',{read: ElementRef}) child: ElementRef;
 
     constructor(private view: ViewRef,
                 private hostElement: ElementRef,
@@ -24,8 +25,10 @@ export class AppComponent implements AfterViewInit {
         // console.log(this.view.context); //ViewRef links to an AppComponent
         console.log(this.hostElement); //Check if ElementRef(h2) belongs to AppComponent
 
-        this.renderer.removeChild(this.hostElement.nativeElement, this.title.nativeElement) //Dont't do this(Structure manipulation with Renderer)!!! Only with 3-ty part component
+        // this.renderer.removeChild(this.hostElement.nativeElement, this.title.nativeElement)       //Dont't do this(Structure manipulation with Renderer)!!! Only with 3-d part component
 
+        this.renderer.removeChild(this.hostElement.nativeElement,this.child.nativeElement)
+        //Dont't do this(Structure manipulation with Renderer)!!!
     }
 
     noClick() {
