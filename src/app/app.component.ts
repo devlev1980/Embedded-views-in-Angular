63945d7f947e711764ff39ef364f5045f36c3edc
yevgenyl <yevgeny@consist.co.il>
2020-03-23 13:38:06 +0200

    change title to 'View container'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,35 +3,15 @@ import {AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild, View
 @Component({
     selector: 'app-root',
     template: `
-        <h1 class="display-1">Embedded views in Angular </h1>
-        <h2 #title>I'm a title</h2>
-        <button (click)="noClick()">Click Me!</button>
-        <app-child #childRef></app-child>
+        <h1 class="display-1">View Container in Angular </h1>
+      
     `,
     styles: []
 })
-export class AppComponent implements AfterViewInit {
-    @ViewChild('title') title: ElementRef;
-    @ViewChild('childRef',{read: ElementRef}) child: ElementRef;
-
-    constructor(private view: ViewRef,
-                private hostElement: ElementRef,
-                private renderer: Renderer2) {
-    }
+export class AppComponent {
 
 
-    ngAfterViewInit(): void {
-        console.log(this.view); //ViewRef Object (ChangeDetection works on this View)
-        // console.log(this.view.context); //ViewRef links to an AppComponent
-        console.log(this.hostElement); //Check if ElementRef(h2) belongs to AppComponent
 
-        // this.renderer.removeChild(this.hostElement.nativeElement, this.title.nativeElement)       //Dont't do this(Structure manipulation with Renderer)!!! Only with 3-d part component
 
-        this.renderer.removeChild(this.hostElement.nativeElement,this.child.nativeElement)
-        //Dont't do this(Structure manipulation with Renderer)!!!
-    }
 
-    noClick() {
-
-    }
 }
