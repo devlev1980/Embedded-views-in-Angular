312ed383d00158a0d5fa6d4eaf66db501c3db54f
yevgenyl <yevgeny@consist.co.il>
2020-03-23 13:51:10 +0200

    using ViewContainer Ref

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,27 @@
-import {AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild, ViewRef} from '@angular/core';
+import {AfterViewInit, Component, ViewChild, ViewContainerRef} from '@angular/core';
+import {NgClass} from "@angular/common";
 
 @Component({
     selector: 'app-root',
     template: `
         <h1 class="display-1">View Container in Angular </h1>
-      
+<!--        <div #container></div>-->
+        <ng-container #container></ng-container> 
+<!--        <h2 [ngClass]="{'container': true}" ></h2>-->
     `,
     styles: []
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+    @ViewChild('container', {read: ViewContainerRef}) container: ViewContainerRef;
+    // @ViewChild(NgClass, {read: ViewContainerRef}) containerRef: ViewContainerRef;
 
+    constructor(private viewContainer: ViewContainerRef) {
+    }
 
-
+    ngAfterViewInit(): void {
+        console.log('View Container', this.viewContainer);
+        console.log(this.container) // See the functions that container exist
+    }
 
 
 }
